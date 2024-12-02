2ff40f533b5d8e91b2eb57666b45c43cee17b112
np03a170120 <czon.herald75@gmail.com>
2024-12-02 18:55:02 +0545

    feat:IIFEs references

--- odin-project/factoryFunction&ModulePattern/index.js
+++ odin-project/factoryFunction&ModulePattern/index.js
@@ -11,6 +11,53 @@ function createUser(name) {
   return { name, discordName, getReputation, giveReputation };
 }
 
+function createPlayer(name, level) {
+  const { getReputation, giveReputation } = createUser(name);
+
+  const increasedLevel = () => level++;
+
+  return { increasedLevel, getReputation, giveReputation };
+}
+
+function createPlayer(name) {
+  const user = createUser(name);
+  const increasedLevel = () => level++;
+
+  return Object.assign({}, user, { increasedLevel });
+}
+
 const person = createUser("Season");
 
-console.log(person);
+console.log(person.getReputation);
+
+function createCirle(raduis) {
+  return {
+    raduis,
+    area() {
+      console.log((22 / 7) * raduis * raduis);
+    },
+  };
+}
+
+const circle1 = createCirle(24);
+
+console.log(circle1);
+
+// IIFES
+// immediately invoking function expression
+
+const calculator = (function () {
+  const add = (a, b) => a + b;
+  const sub = (a, b) => a - b;
+
+  return { add, sub };
+})();
+
+calculator.add(2, 7);
+
+const Greet = (function () {
+  const sayHi = (message) => console.log(message);
+  return { sayHi };
+})();
+
+Greet.sayHi("tell him");
